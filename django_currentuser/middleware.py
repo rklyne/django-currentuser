9dd2a2d217a2cfa0ac7406210465c2af29cf3fd3
Ronan Klyne <ronan.klyne@virtualstock.co.uk>
2018-08-21 16:04:46 +0100

    Null the user after a request

--- django_currentuser/middleware.py
+++ django_currentuser/middleware.py
@@ -28,6 +28,12 @@ class ThreadLocalUserMiddleware(object):
         # request.user (non-data descriptor)
         _do_set_current_user(lambda self: getattr(request, 'user', None))
 
+    def process_response(self, request, response):
+        _set_current_user(None)
+
+    def process_exception(self, request, response):
+        _set_current_user(None)
+
 
 def get_current_user():
     current_user = getattr(_thread_locals, USER_ATTR_NAME, None)
